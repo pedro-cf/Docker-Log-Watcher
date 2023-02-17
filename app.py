75b88b196c9334768992b6f0ccac8767fd114c54
pedro-cf <pedro_fernandes@live.com>
2023-02-17 19:31:32 +0000

    hot

--- app.py
+++ app.py
@@ -4,18 +4,24 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 client = docker.from_env()
 
-@app.route('/')
+
+@app.route("/")
 def index():
     containers = client.containers.list()
     container_names = [container.name for container in containers]
-    return render_template('index.html', container_names=container_names)
+    return render_template("index.html", container_names=container_names)
 
-@app.route('/logs')
+
+@app.route("/logs")
 def logs():
-    container_name = request.args.get('container_name')
-    container = client.containers.get(container_name)
-    logs = container.logs(tail=100, stream=False).decode('utf-8')
-    return logs # jsonify(logs=logs)
+    try:
+        container_name = request.args.get("container_name")
+        container = client.containers.get(container_name)
+        logs = container.logs(tail=500, stream=False).decode("utf-8")
+        return logs  # jsonify(logs=logs)
+    except (docker.errors.NotFound, docker.errors.NullResource):
+        return ""
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
